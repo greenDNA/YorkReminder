090ef4891c64205cf284fbdc5f097dad1882cda5
greenDNA <trayvont.york@gmail.com>
2017-03-13 17:46:40 -0400

    Created methods to manage operations through use of switch statements, and left comments on future goals at head of source code, as well as leaving notes of vulnerabilities in the code for failures that need checks and fixing

--- src/Reminder.java
+++ src/Reminder.java
@@ -10,11 +10,13 @@ public class Reminder {
     String reminderName;
     Date reminderDate;
     Boolean reminderStatus;
+    int reminderPosition;
 
     /**
      * Constructors, initializing values of variables
      */
     public Reminder(Scanner uInput){
+        System.out.println("Please enter information for a new reminder!");
         //to be finished, create a reminder without any parameters
         System.out.print("\nEnter name of reminder: ");
         reminderName = uInput.nextLine();
@@ -40,11 +42,24 @@ public class Reminder {
         return reminderStatus;
     }
 
+    public int getReminderPosition() {
+        return reminderPosition;
+    }
+
+    public Reminder setReminderPosition(int reminderPosition) {
+        this.reminderPosition = reminderPosition;
+        return this;
+    }
+
+    public void setReminderStatus(Boolean reminderStatus) {
+        this.reminderStatus = reminderStatus;
+    }
+
     @Override
     /**
      * Override this to control what gets printed from a System.out.print() call or any variant of that
      */
     public String toString(){
-        return "Name: "+reminderName + "\nCreated: " +reminderDate + "\nStatus: "+reminderStatus;
+        return "Name: "+reminderName + "\nCreated: " +reminderDate + "\nStatus: "+reminderStatus + "\nPosition: " + reminderPosition;
     }
 }
--- src/ReminderManager.java
+++ src/ReminderManager.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 /**
  * Created by Trayvont on 3/12/2017.
- *
+ * Security checks have to be put in place************
+ * Support for saving lists and loading lists
+ * Support for personalized lists, password protected perhaps
  * Class that will be managing the reminders entered and tracking their attributes
  */
 public class ReminderManager {
@@ -19,24 +21,35 @@ public class ReminderManager {
         Scanner uInput = new Scanner(System.in);
 
         while(running){
-            System.out.println("Please enter information for a new reminder!");
-            addReminder(new Reminder(uInput));
-            printReminderList();
-            System.out.println("\nWould you like to add another? (Y/N)");
-            //For some reason if I use uInput.next() on the following line an input bug occurs where the program defaults
-            //the reminder name to a blank value if you respond to the (Y/N) with one word
-            //to get around it, respond with one word, use a space(perhaps it delimits) and after the space use the words you would
-            //like to be stored as the reminder's name. nextLine() solves the issue stated above when used in the below statement
-            switch (uInput.nextLine()){
-                case "n":
-                case "N":
+            printMenuList();
+            switch (Integer.parseInt(uInput.nextLine())){
+
+                case 1:
+                    addReminder(new Reminder(uInput).setReminderPosition(reminderList.size()+1));
+                    break;
+                case 2:
+                    printReminderList();
+                    break;
+                case 3:
+                    System.out.println("Enter the position of the reminder you would like to change the status of.");
+                    getReminderFromList(Integer.parseInt(uInput.nextLine())-1);
+                    break;
+                case 4:
                     running = false;
+                    System.out.println("Exiting...");
                     break;
                 default:
                     break;
             }
+            //System.out.println("\nWould you like to add another? (Y/N)");
+            //For some reason if I use uInput.next() on the following line an input bug occurs where the program defaults
+            //the reminder name to a blank value if you respond to the (Y/N) with one word
+            //to get around it, respond with one word, use a space(perhaps it delimits) and after the space use the words you would
+            //like to be stored as the reminder's name. nextLine() solves the issue stated above when used in the below statement
+            //nextInt and next does not consume the newline, so using nextInt and parsing it into an integer is the best way to solve
+            //this oppose to dirty fixes
         }
-        System.out.println("Thank you for using YorkReminder!");
+        System.out.println("\nThank you for using YorkReminder!");
     }
 
     /**
@@ -52,8 +65,35 @@ public class ReminderManager {
      * Method to print the entire main ReminderList object
      */
     public void printReminderList(){
+        System.out.println("Here is a list of your Reminder(s)!");
         for(Reminder i : reminderList){
+    //add a way to track position in the reminder list, thinking of using an integer and iterate with int++ or ++int perhaps
             System.out.println("\n"+i);
         }
+        System.out.println();
+    }
+    public void printMenuList(){
+        System.out.println("Enter the appropriate choice to carry out one of the operations below.\n");
+        System.out.println("1) - Create a new reminder.");
+        System.out.println("2) - View a list of your reminders.");
+        System.out.println("3) - Change status of your reminder.");
+        System.out.println("4) - Exit program.");
+        System.out.print("\nEnter your choice: ");
+    }
+
+    /**
+     * Cleanup this code, gets Reminder from the reminderList ArrayList and then modifies the boolean value which
+     * represents whether or not the reminder task has been completed
+     * Vulnerability if user enters a value greater than the size of the ArrayList, check for OUTOFBOUNDS
+     * @param choice
+     */
+    public void getReminderFromList(int choice){
+        System.out.println("Changing the status of reminder at position " + (choice+1) +".");
+        if( reminderList.get(choice).getReminderStatus() == false){
+            reminderList.get(choice).setReminderStatus(true);
+        }else{
+            reminderList.get(choice).setReminderStatus(false);
+        }
+        return;
     }
 }
