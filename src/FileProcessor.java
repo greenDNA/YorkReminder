a00b2a428ff47559d0b4d71c59cc72b5ee807f4b
greenDNA <trayvont.york@gmail.com>
2017-03-14 22:58:23 -0400

    Begin work on working with files

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
 
 /**
  * Created by Trayvont on 3/12/2017.
@@ -8,9 +11,23 @@ import java.io.File;
  */
 public class FileProcessor {
     File myFile;
+    BufferedReader br;
+    PrintWriter pw;
+    String buffer;
 
+    /**
+     * Introductory method to readin from a file into a String buffer
+     */
     public FileProcessor(){
-
+        myFile = new File("Test.txt");
+        try{
+            if(myFile.exists()){
+                br = new BufferedReader(new FileReader(myFile));
+                buffer = br.readLine();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public FileProcessor(File inFile){
